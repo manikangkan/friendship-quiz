0b63b53e2de530f8c79000480c1c78bfc09f1c0d
manikangkandas global <manikangkan.das@yahoo.com>
2021-11-09 20:14:23 +0530

    done

--- db/connect.js
+++ db/connect.js
@@ -1,24 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const ACCOUNT_NAME = "kal"; //PUT MONGODB USERNAME HERE
+const PASS = "lalu"; //PUT MONGODB PASSWORD HERE
 
-const ACCOUNT_NAME = '';   //PUT MONGODB USERNAME HERE
-const PASS = '';           //PUT MONGODB PASSWORD HERE
+const uri1 = `mongodb+srv://${ACCOUNT_NAME}:${PASS}@cluster0.p7xal.mongodb.net/quizdb?retryWrites=true&w=majority`;
 
-const uri1 =
-    `mongodb+srv://${ACCOUNT_NAME}:${PASS}@cluster0.p7xal.mongodb.net/quizdb?retryWrites=true&w=majority`;
-
-mongoose.connect(uri1, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log('userDB OPEN!!!');
-    })
-    .catch((err) => {
-        console.log('ERROR CONNECTING');
-        console.log(err);
-    });
-    
-    
-
-
+mongoose
+  .connect(uri1, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("userDB OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("ERROR CONNECTING");
+    console.log(err);
+  });
