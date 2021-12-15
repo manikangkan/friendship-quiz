e883ed23c2144c4e2ee6bd96fd19428bd616bc0d
atm92 <92220835+atm92@users.noreply.github.com>
2021-12-16 04:03:53 +0530

    avatar configs

--- models/user.js
+++ models/user.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    cookedQs : {
-        type: []
-    },
-    gender : String,
-    date : Date,
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	cookedQs: {
+		type: [],
+	},
+	gender: String,
+	date: Date,
+	avatarCode: String,
 });
 
 const User = mongoose.model('User', userSchema);
